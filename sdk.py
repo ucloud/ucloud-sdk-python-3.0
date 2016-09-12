1f9d72e6ad32dcae6783224999db34045c7f7f84
wentianle <wentianle@ucloud.cn>
2016-09-12 11:47:59 +0800

    init python 3.0 sdk

---
+++ sdk.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+import hashlib, json, http.client
+import urllib.parse
+import urllib.request, urllib.parse, urllib.error
+import sys
+from config import *
+
+
+class UCLOUDException(Exception):
+    def __str__(self):
+        return "Error"
+
+
+def _verfy_ac(private_key, params):
+    items = list(params.items())
+    items.sort()
+
+    params_data = ""
+    for key, value in items:
+        params_data = params_data + str(key) + str(value)
+
+    params_data = params_data+private_key
+    
+    '''use sha1 to encode keys'''
+    hash_new = hashlib.sha1()
+    hash_new.update(params_data.encode(encoding="utf-8"))
+    hash_value = hash_new.hexdigest()
+    return hash_value
+
+
+class UConnection(object):
+    def __init__(self, base_url):
+        self.base_url = base_url
+        o = urllib.parse.urlsplit(base_url)
+        if o.scheme == 'https':
+            self.conn = http.client.HTTPSConnection(o.netloc)
+        else:
+            self.conn = http.client.HTTPConnection(o.netloc)
+
+    def __del__(self):
+        self.conn.close()
+
+    def get(self, resouse, params):
+        resouse += "?" + urllib.parse.urlencode(params)
+        print(("%s%s" % (self.base_url, resouse)))
+        self.conn.request("GET", resouse)
+        response = json.loads(self.conn.getresponse().read().decode(encoding='utf-8'))
+        return response
+
+
+class UcloudApiClient(object):
+    # 添加 设置 数据中心和  zone 参数
+    def __init__(self, base_url, public_key, private_key):
+        self.g_params = {}
+        self.g_params['PublicKey'] = public_key
+        self.private_key = private_key
+        self.conn = UConnection(base_url)
+
+    def get(self, uri, params):
+        # print params
+        _params = dict(self.g_params, **params)
+
+        if project_id : 
+            _params["ProjectId"] = project_id
+
+        _params["Signature"] = _verfy_ac(self.private_key, _params)
+        return self.conn.get(uri, _params)
---
+++ send_sms.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from sdk import UcloudApiClient
+from config import *
+import sys
+import json
+
+#实例化 API 句柄
+
+if __name__=='__main__':
+    arg_length = len(sys.argv)
+    if arg_length == 1 or arg_length == 2 :
+        print('./sendsms.py "1377777777777|137888888888" "测试短信"')
+        sys.exit()
+    ApiClient = UcloudApiClient(base_url, public_key, private_key)
+    phones = sys.argv[1].split('|')
+    Parameters={
+            "Action":"SendSms",
+            "Content":sys.argv[2]
+            }
+    if sys.platform == 'win32' : 
+        Parameters={
+                "Action":"SendSms",
+                "Content": "windows 命令行测试不支持传中文请谅解"
+                }
+
+    for i in range(len(phones)):
+        Parameters['Phone.'+str(i)] = phones[i]
+    response = ApiClient.get("/", Parameters );
+    print((json.dumps(response, sort_keys=True, indent=4, separators=(',', ': '))))
